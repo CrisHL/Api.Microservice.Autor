15da4bfe9d02f3b5b8a48bc841ff96031923800d
Cristian Hernández <crishdez2341@gmail.com>
2024-06-04 00:42:25 -0600

    Actualizacion de conexion a bd

---
+++ Api.Microservice.Autor/Migrations/20240604062313_AutorLibro.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Api.Microservice.Autor.Migrations
+{
+    /// <inheritdoc />
+    public partial class AutorLibro : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "AutorLibros",
+                columns: table => new
+                {
+                    AutorLibroId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Nombre = table.Column<string>(type: "longtext", nullable: false),
+                    Apellido = table.Column<string>(type: "longtext", nullable: false),
+                    FechaNacimiento = table.Column<DateTime>(type: "datetime", nullable: true),
+                    AutorLibroGuid = table.Column<string>(type: "longtext", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AutorLibros", x => x.AutorLibroId);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "GradosAcademicos",
+                columns: table => new
+                {
+                    GradoAcademicoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Nombre = table.Column<string>(type: "longtext", nullable: false),
+                    CentroAcademico = table.Column<string>(type: "longtext", nullable: false),
+                    FechaGrado = table.Column<DateTime>(type: "datetime", nullable: true),
+                    AutorLibroId = table.Column<int>(type: "int", nullable: false),
+                    GradoAcademicoGuid = table.Column<string>(type: "longtext", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GradosAcademicos", x => x.GradoAcademicoId);
+                    table.ForeignKey(
+                        name: "FK_GradosAcademicos_AutorLibros_AutorLibroId",
+                        column: x => x.AutorLibroId,
+                        principalTable: "AutorLibros",
+                        principalColumn: "AutorLibroId",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GradosAcademicos_AutorLibroId",
+                table: "GradosAcademicos",
+                column: "AutorLibroId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GradosAcademicos");
+
+            migrationBuilder.DropTable(
+                name: "AutorLibros");
+        }
+    }
+}
